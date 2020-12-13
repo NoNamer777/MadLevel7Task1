e1bf0f75cb56efda9682d1e5d21d77c2cc1a44e1
Oscar Wellner <o.wellner@telfort.nl>
2020-12-13 23:10:21 +0100

    Step 4: created the 'profile' fragment layout and logic

--- app/src/main/java/com/nonamer777/madlevel7task1/ui/ProfileFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel7task1/ui/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.nonamer777.madlevel7task1.ui
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.nonamer777.madlevel7task1.R
 import com.nonamer777.madlevel7task1.databinding.FragmentProfileBinding
+import java.net.URI
 
 /**
  * A [Fragment] subclass that shows the created profile.
@@ -14,6 +18,8 @@ class ProfileFragment: Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
 
+    private val profileViewModel: ProfileViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,5 +27,30 @@ class ProfileFragment: Fragment() {
         binding = FragmentProfileBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeProfile()
+    }
+
+    private fun observeProfile() {
+        profileViewModel.getProfile()
+
+        profileViewModel.profile.observe(viewLifecycleOwner, {
+            val profile = it
+
+            binding.labelName.text = getString(
+                R.string.label_name,
+                profile.firstName,
+                profile.lastName
+            )
+            binding.paragraphDescription.text = profile.description
+
+            if (profile.imageUri!!.isNotEmpty()) {
+                binding.imgProfilePic.setImageURI(Uri.parse(profile.imageUri))
+            }
+        })
     }
 }
